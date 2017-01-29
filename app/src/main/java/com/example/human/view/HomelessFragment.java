329525382e11196ae8f2506f95de0f73cea77ecc
Asia <gibsonjasia@gmail.com>
2017-01-29 12:25:55 -0500

    rv added

--- app/src/main/java/com/example/human/view/HomelessFragment.java
+++ app/src/main/java/com/example/human/view/HomelessFragment.java
@@ -2,17 +2,24 @@ package com.example.human.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.human.R;
+import com.example.human.asia.rv.HlAdapter;
 
 public class HomelessFragment extends Fragment {
+
+    RecyclerView rv;
+    HlAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.homeless_service_fragment, container, false);
+
+
     }
 
 }
